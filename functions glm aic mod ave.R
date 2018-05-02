7d233bc796ce10d2ce596706dfca5a64a4cbf703
Daniel Weinberger <stopaction13@gmail.com>
2018-05-02 13:23:03 +1000

    Initial version
    AIC-based model averaging, with simulation-based prediction intevrals

---
+++ functions glm aic mod ave.R
@@ -0,0 +1,70 @@
+#FOR BRAZIL: DETREND
+getTrend <- function(covar_vector, data) {
+  new_data <- data
+  new_data[c('bs1', 'bs2', 'bs3', 'bs4')] <- 0
+  new_data$month_i <- as.factor(1)
+  trend <- predict(glm(covar_vector~month_i + ., family = 'gaussian', data = data), type = 'response', newdata = new_data) #month_i is first to be the reference.
+  names(trend) <- NULL
+  return(trend)
+}
+
+DoSTL_trend <- function(new,t.windows,s.windows) {
+  trend <- as.data.frame(matrix(NA, nrow=nrow(new), ncol=ncol(new)))
+  for (j in 1:ncol(new)) {
+    ts <- ts(new[,j], frequency=12)
+    trend[,j] <- as.vector(stl(ts, s.window=s.windows, t.window=t.windows)[[1]][,2]) 
+  }
+  colnames(trend) <- c(paste(colnames(new),".trend.",t.windows,sep=""))
+  return(trend)
+}
+
+glm.fun<-function(ds.fit.fun){
+  mod1<-glm.nb(y ~ . , data=ds.fit.fun)
+  aic.test<-AIC( mod1)
+  V<- vcov( mod1)
+  coef1<-coef(mod1)
+  theta<-mod1$theta
+  pred.mean<- exp(predict(mod1, newdata=ds.fit.fun))
+  test.var<-names(ds.fit.fun)[length(ds.fit.fun)]
+  glm.out<-list(aic.test, V, coef1, pred.mean,test.var,theta) #save output in a named list
+  names(glm.out)<-c('aic.test','V','coef1','pred.mean', 'test.var','theta')
+  return(glm.out)
+}
+
+#Samples for parameter uncertainty piece
+param.uncertainty<-function(param.ds){
+  incl.names<-c('m1','m2','m3','m4','m5','m6', 'm7','m8','m9','m10','m11','pandemic', param.ds$test.var )
+  keep.cols<-which(names(data.fit) %in% incl.names )
+  covars3<-cbind.data.frame(rep(1, times=nrow(data.fit)), data.fit[,keep.cols])
+  names(covars3)[1]<-"Intercept"
+  N.draws.stage1<- round(sqrt(param.ds$Nsamps))
+  if(N.draws.stage1>0){
+    #In stage 1, generate N1 samples from parameter distribution and multiply by covariates to get pred
+    pred.coefs<- mvrnorm(n = N.draws.stage1, mu=param.ds$coef1, Sigma=param.ds$V)
+    if(N.draws.stage1==1){preds.stage1<- as.matrix(covars3) %*% pred.coefs
+    }else{  
+      preds.stage1<- as.matrix(covars3) %*% t(pred.coefs) #the number of columns in the first matrix has to be equal to the number of rows in the second matrix 
+    }
+  }else{
+    preds.stage1<-NA
+  }
+  stage1.results<-list(N.draws.stage1, preds.stage1,param.ds$Nsamps,param.ds$theta)
+  names(stage1.results)<-c('N.draws.stage1','preds.stage1',"Nsamps",'theta')
+  return(stage1.results)
+}
+
+#Second stage sampling to add on observation uncertainty from negbin distribution
+obs.uncertainty<-function(stage1.ds){
+  N.draws.stage1<-stage1.ds$N.draws.stage1
+  Nsamps<-stage1.ds$Nsamps
+  if(Nsamps>0){
+  preds.stage1<-stage1.ds$preds.stage1
+  #In stage 2, take prediction from stage 1 and sample N2 timesfrom negbin
+  N.draws.stage2<-rmultinom(1,prob=rep(1/N.draws.stage1, times=N.draws.stage1), size=Nsamps)
+  #replicate mean vector N.draws.stage2 times 
+  preds.stage1.rep<-preds.stage1[,rep(seq_len(ncol(preds.stage1)), as.vector(N.draws.stage2)  )]
+  preds.stage1.rep<-matrix(preds.stage1.rep, nrow=nrow(preds.stage1), ncol=Nsamps)
+  preds.stage2 <- rnegbin(exp(preds.stage1.rep), theta = stage1.ds$theta)
+  preds.stage2 <-matrix(preds.stage2, nrow=nrow(preds.stage1.rep), ncol=ncol(preds.stage1.rep))
+  }
+}
