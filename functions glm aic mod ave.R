c64053427465d5a0bae0eaf41e070a632311175f
Daniel Weinberger <stopaction13@gmail.com>
2018-05-10 14:33:50 -0400

    Update functions glm aic mod ave.R

--- functions glm aic mod ave.R
+++ functions glm aic mod ave.R
@@ -55,7 +55,8 @@ fixed.effects<-paste(names(ds.fit.fun)[-1], collapse="+")
 ds.fit.fun$obs<-as.factor(1:nrow(ds.fit.fun))
 form1<-as.formula(paste0('y~', fixed.effects, "+ (1|obs)" ))
 mod1<-glmer(form1,data=ds.fit.fun[pre.index,], family='poisson',control=glmerControl(optimizer="bobyqa",
-                                                                                      optCtrl=list(maxfun=2e5)) )pred.mean<-predict(mod1, newdata=ds.fit.fun,re.form=NA )
+                                                                                      optCtrl=list(maxfun=2e5)) )
+pred.mean<-predict(mod1, newdata=ds.fit.fun,re.form=NA )
 aic.test<-AIC( mod1)
 test.var<-   attributes(ds.fit.fun)$comment  #THIS IS DIFFERENT FOR BIVARIATE
 glm.out<-list(pred.mean,ds.fit.fun, mod1,aic.test, test.var) #save output in a named list
