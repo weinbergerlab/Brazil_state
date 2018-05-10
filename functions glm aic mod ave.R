6f15e829961c1a41345ca56d98d9ede7caad3987
Daniel Weinberger <stopaction13@gmail.com>
2018-05-09 22:19:02 -0400

    fix incl.covars, removing y

--- functions glm aic mod ave.R
+++ functions glm aic mod ave.R
@@ -64,15 +64,15 @@ glm.fun<-function(ds.fit.fun){
 #Samples for parameter uncertainty piece
 param.uncertainty<-function(param.ds){
     if(param.ds$test.var!='nocovars'){if (season.control=='dummy'){
-  incl.names<-c('y','m1','m2','m3','m4','m5','m6', 'm7','m8','m9','m10','m11','pandemic' ) #NULL MODEL
+  incl.names<-c('m1','m2','m3','m4','m5','m6', 'm7','m8','m9','m10','m11','pandemic' ) #NULL MODEL
 }else{
-  incl.names<-c('y','sint1','cost1','pandemic' ) #NULL MODEL
+  incl.names<-c('sint1','cost1','pandemic' ) #NULL MODEL
 }
 }else{
   if (season.control=='dummy'){
-    incl.names<-c('y','m1','m2','m3','m4','m5','m6', 'm7','m8','m9','m10','m11','pandemic', param.ds$test.var  )
+    incl.names<-c('m1','m2','m3','m4','m5','m6', 'm7','m8','m9','m10','m11','pandemic', param.ds$test.var  )
   }else{
-    incl.names<-c('y','sint1','cost1','pandemic', param.ds$test.var  ) 
+    incl.names<-c('sint1','cost1','pandemic', param.ds$test.var  ) 
   }
 }
   keep.cols<-which(names(data.fit) %in% incl.names )
